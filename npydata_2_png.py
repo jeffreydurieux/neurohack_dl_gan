36b0191dc6baf1acd33836b69962fdb83d74ff50
Jeffrey Durieux <durieux.jeffrey@gmail.com>
2019-08-06 14:06:05 -0700

    Add script npydata_2_png.py
    This is the initial commit for the python script that converts numpy arrays to png files

---
+++ npydata_2_png.py
@@ -0,0 +1,21 @@
+# script for converting np.arrays to png images
+
+import os 
+import numpy as np
+import imageio
+
+os.chdir('/home/ubuntu/data/')
+
+# load data, shape = (N , 311, 311), where N is number of subjects
+data = np.load('hcp_dat.npy')
+
+
+# for loop over the matrices and write as png to pngimages directory
+os.chdir('/home/ubuntu/data/pngimages/')
+
+print('Saving png images')
+
+idx = np.shape(data)
+for ii in range(idx[0]):
+    imageio.imwrite('{}.png'.format(ii), data[ii])
+    print('Image {} done'.format(ii))
