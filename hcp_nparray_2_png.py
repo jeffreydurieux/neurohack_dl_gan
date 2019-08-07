080f2f14b227fbc0700af1d8fa49037a32d3b86c
Jeffrey Durieux <durieux.jeffrey@gmail.com>
2019-08-07 10:35:24 -0700

    Changed paths in script for aws cluster

--- hcp_nparray_2_png.py
+++ hcp_nparray_2_png.py
@@ -16,22 +16,17 @@ import os
 import numpy as np
 import imageio
 
-os.chdir('/Users/jeffreydurieux/Desktop/')
-
-dat = np.load('hcp_dat.npy')
-
-np.shape(dat)
-dat = dat[0,] # subjects X axis0 X axis1 X axis2
-
-idx = np.shape(dat)
 
 
-for subject in range(idx[0]):
-    os.chdir('/Users/jeffreydurieux/Desktop/pngimages/')
+for subject in range(2):
+    os.chdir('/home/ubuntu/data/hcp_npy_data/')
+    datasubject = np.load('hcp_data_{}.npy'.format(subject))
+    
+    os.chdir('/home/ubuntu/data/hcp_pngimages/')
     os.makedirs('subject{}'.format(subject))
     os.chdir('subject{}'.format(subject))
     
-    datasubject = dat[subject]
+    
     idxim = np.shape(datasubject)
     
     os.makedirs('axis0')
@@ -39,13 +34,13 @@ for subject in range(idx[0]):
     os.makedirs('axis2')
     
     for ax0 in range(idxim[0]):
-        os.chdir('/Users/jeffreydurieux/Desktop/pngimages/subject{}/axis0/'.format(subject))
+        os.chdir('/home/ubuntu/data/hcp_pngimages/subject{}/axis0/'.format(subject))
         imageio.imwrite('{}.png'.format(ax0), datasubject[ax0, :, :])
     for ax1 in range(idxim[1]):
-        os.chdir('/Users/jeffreydurieux/Desktop/pngimages/subject{}/axis1/'.format(subject))
+        os.chdir('/home/ubuntu/data/hcp_pngimages/subject{}/axis1/'.format(subject))
         imageio.imwrite('{}.png'.format(ax1), datasubject[:, ax1, :])
     for ax2 in range(idxim[2]):
-        os.chdir('/Users/jeffreydurieux/Desktop/pngimages/subject{}/axis2/'.format(subject))
+        os.chdir('/home/ubuntu/data/hcp_pngimages/subject{}/axis2/'.format(subject))
         imageio.imwrite('{}.png'.format(ax2), datasubject[:, :, ax2])
 
 
