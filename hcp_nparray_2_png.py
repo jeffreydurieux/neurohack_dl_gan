6e175b126eb30901a7b76458a469e690a4c9fc54
Jeffrey Durieux <durieux.jeffrey@gmail.com>
2019-08-07 10:39:56 -0700

    made a wrapper function for the hcp_nparray_2_png function

--- hcp_nparray_2_png.py
+++ hcp_nparray_2_png.py
@@ -16,31 +16,34 @@ import os
 import numpy as np
 import imageio
 
-
-
-for subject in range(2):
-    os.chdir('/home/ubuntu/data/hcp_npy_data/')
-    datasubject = np.load('hcp_data_{}.npy'.format(subject))
+def run_create_png(subjectrange):
+    '''
+    function for creating png images based on subjectrange (max = 1112)
+    '''
     
-    os.chdir('/home/ubuntu/data/hcp_pngimages/')
-    os.makedirs('subject{}'.format(subject))
-    os.chdir('subject{}'.format(subject))
+    for subject in range(2):
+        os.chdir('/home/ubuntu/data/hcp_npy_data/')
+        datasubject = np.load('hcp_data_{}.npy'.format(subject))
+    
+        os.chdir('/home/ubuntu/data/hcp_pngimages/')
+        os.makedirs('subject{}'.format(subject))
+        os.chdir('subject{}'.format(subject))
     
     
-    idxim = np.shape(datasubject)
+        idxim = np.shape(datasubject)
     
-    os.makedirs('axis0')
-    os.makedirs('axis1')
-    os.makedirs('axis2')
+        os.makedirs('axis0')
+        os.makedirs('axis1')
+        os.makedirs('axis2')
     
-    for ax0 in range(idxim[0]):
-        os.chdir('/home/ubuntu/data/hcp_pngimages/subject{}/axis0/'.format(subject))
-        imageio.imwrite('{}.png'.format(ax0), datasubject[ax0, :, :])
-    for ax1 in range(idxim[1]):
-        os.chdir('/home/ubuntu/data/hcp_pngimages/subject{}/axis1/'.format(subject))
-        imageio.imwrite('{}.png'.format(ax1), datasubject[:, ax1, :])
-    for ax2 in range(idxim[2]):
-        os.chdir('/home/ubuntu/data/hcp_pngimages/subject{}/axis2/'.format(subject))
-        imageio.imwrite('{}.png'.format(ax2), datasubject[:, :, ax2])
+        for ax0 in range(idxim[0]):
+            os.chdir('/home/ubuntu/data/hcp_pngimages/subject{}/axis0/'.format(subject))
+            imageio.imwrite('{}.png'.format(ax0), datasubject[ax0, :, :])
+        for ax1 in range(idxim[1]):
+            os.chdir('/home/ubuntu/data/hcp_pngimages/subject{}/axis1/'.format(subject))
+            imageio.imwrite('{}.png'.format(ax1), datasubject[:, ax1, :])
+        for ax2 in range(idxim[2]):
+            os.chdir('/home/ubuntu/data/hcp_pngimages/subject{}/axis2/'.format(subject))
+            imageio.imwrite('{}.png'.format(ax2), datasubject[:, :, ax2])
 
 
