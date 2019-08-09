0e5bd44b3536af16d85cb2e247f20952548d19c7
Anna Truzzi <46749720+AnnaTruzzi@users.noreply.github.com>
2019-08-09 12:30:59 -0700

    Removed the /young folder form the input directory

--- 1_preprocess_niftified_pngs.sh
+++ 1_preprocess_niftified_pngs.sh
@@ -2,4 +2,4 @@ set -x
 subj=$1
 
 mkdir -p /data/analysis_young/"$subj"_derivative/
-bet /data/young/$subj /data/analysis_young/"$subj"_derivative/"$subj"_brain
+bet /data/$subj /data/analysis_young/"$subj"_derivative/"$subj"_brain
