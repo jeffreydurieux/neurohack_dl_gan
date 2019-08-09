f583fa1188870a30017f9460c7a8bddd7ac7b476
Anna Truzzi <46749720+AnnaTruzzi@users.noreply.github.com>
2019-08-09 12:39:33 -0700

    Update 1_preprocess_niftified_pngs.sh

--- 1_preprocess_niftified_pngs.sh
+++ 1_preprocess_niftified_pngs.sh
@@ -1,5 +1,5 @@
 set -x
 subj=$1
 
-mkdir -p /data/analysis_young/"$subj"_derivative/
-bet /data/$subj /data/analysis_young/"$subj"_derivative/"$subj"_brain
+#mkdir -p /data/analysis_young/"$subj"_derivative/
+bet /data/$subj /data/"$subj"_derivative/"$subj"_brain
